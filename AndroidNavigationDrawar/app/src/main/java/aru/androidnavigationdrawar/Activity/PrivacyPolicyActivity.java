9421251ed0931a695ee2a39ff1c72802c1b9cf7b
Aru <rupeshgohel007@gmail.com>
2018-04-17 18:05:08 +0530

    change url of profile image and background

---
+++ AndroidNavigationDrawar/app/src/main/java/aru/androidnavigationdrawar/Activity/PrivacyPolicyActivity.java
@@ -0,0 +1,32 @@
+package aru.androidnavigationdrawar.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import aru.androidnavigationdrawar.R;
+
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            // finish the activity
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ AndroidNavigationDrawar/app/src/main/java/aru/androidnavigationdrawar/Utility/Utils.java
@@ -0,0 +1,14 @@
+package aru.androidnavigationdrawar.Utility;
+
+public class Utils {
+
+    public static final String HOME = "Home";
+    public static final String PHOTO = "Photos";
+    public static final String MOVIW = "Moview";
+    public static final String NOTIFICATION = "Notification";
+    public static final String SETTING = "Setting";
+    public static final String ABOUTUS = "Aboutus";
+    public static final String P_POLICY = "Private_Policy";
+    public  static final String urlProfileImg = "https://lh3.googleusercontent.com/eCtE_G34M9ygdkmOpYvCag1vBARCmZwnVS6rS5t4JLzJ6QgQSBquM0nuTsCpLhYbKljoyS-txg";
+
+}
